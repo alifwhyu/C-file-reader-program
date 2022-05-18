24a9ecf81df721f436bbcd9c6e592e4eabca2aeb
alifwhyu <101910498+alifwhyu@users.noreply.github.com>
2022-05-18 08:55:29 +0700

    Update readfile.c

--- readfile.c
+++ readfile.c
@@ -8,7 +8,7 @@ int main()
 
     if ((fptr = fopen("file.txt", "r")) == NULL)
     {
-        printf("Error! opening file");
+        printf("Terjadi kesalahan dalam membuka file");
         // exit from program if file pointer returns NULL.
         exit(1);         
     }
@@ -16,7 +16,7 @@ int main()
     // read the text until newline 
     fscanf(fptr,"%[^\n]", c);
 
-    printf("Data from the file:\n%s", c);
+    printf("Data dari file:\n%s", c);
     fclose(fptr);
     
     return 0;
